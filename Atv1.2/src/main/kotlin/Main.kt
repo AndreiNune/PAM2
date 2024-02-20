5157e2f6452d05d360492ee45142e8342cd63247
AndreiNune <142557578+AndreiNune@users.noreply.github.com>
2024-02-19 22:39:11 -0300

    Atividade 3 finalizada

---
+++ Atv1.2/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var a: Int
+    var b = 2 // Tipo inferido
+    a = 10
+    print(a+b)
+}
